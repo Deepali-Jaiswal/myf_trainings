7a7bd139f497ae47e05383e1086ed20a61910f86
Dhananjay <dhananjay@MyfAdmins-MacBook-Air.local>
2024-04-18 12:55:20 +0530

    login completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import './App.css';
 import Login from './Login';
 import Signup from './Signup';
 import Home from "./Home";
-
 
 function App() {
   return (
